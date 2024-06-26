0adc4810b28bd67d723221da65e98b7c8d67e3d4
Michael-shambel <Michaelshambel10@gmail.com>
2024-06-26 21:37:12 +0000

    task 0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,7 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     setTimeout(() => {
-      resolve("API response data");
+      resolve('API response data');
     }, 1000);
   });
 }
