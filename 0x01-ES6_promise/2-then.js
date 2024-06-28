e53fd9cb5bfcf0eec77d3fd95eb5e9f055c58123
Michael-shambel <Michaelshambel10@gmail.com>
2024-06-28 15:35:28 +0000

    task 231

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,5 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  promise
+  return promise
     .then(() => {
       console.log('Got a response from the API');
       return {
@@ -7,5 +7,8 @@ export default function handleResponseFromAPI(promise) {
         body: 'success',
       };
     })
-    .catch(() => new Error());
+    .catch(() => {
+      console.log('Got a response from the API');
+      return new Error();
+    });
 }
