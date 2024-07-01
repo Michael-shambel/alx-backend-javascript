27c8b760c8433ce69d3c76fc16663bf02109d0b8
michael-shambel <Michaelshambel10@gmail.com>
2024-07-02 01:43:51 +0300

    task 5 up

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,13 +1,14 @@
 export default class Building {
   constructor(sqft) {
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessege !== 'function') {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+    }
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  static evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
