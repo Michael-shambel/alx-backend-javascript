0d1e7a08290b1bc2c7a9e2d8bb2f64ee5ded3c3f
michael-shambel <Michaelshambel10@gmail.com>
2024-07-02 12:41:14 +0300

    task 2 up

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,3 @@
-export default function getStudentByLocation(students, location) {
-  return students.filter((student) => student.location === location);
+export default function getStudentByLocation(students, place) {
+  return students.filter((student) => student.location === place);
 }
