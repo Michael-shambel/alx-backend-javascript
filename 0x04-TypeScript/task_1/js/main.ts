d12cf3dd9efc86046505587e4d25224b2af89f4d
michael-shambel <Michaelshambel10@gmail.com>
2024-07-03 14:58:44 +0300

    task 3

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -10,3 +10,12 @@ interface Teacher {
 interface Directors extends Teacher {
   numberOfReports: number;
 }
+
+const printTeacher = (firstName, lastName) => {
+	const firstShortName = firstName.charAt(0).toUpperCase();
+	return `${firstShortName}. ${lastName}`;
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
