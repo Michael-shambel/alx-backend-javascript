a59dff96f48dda1ab1c49585ba580517cdca31df
michael-shambel <Michaelshambel10@gmail.com>
2024-08-21 13:14:50 +0300

    task 7

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+    const dbpath = process.argv[2];
+    try {
+      const output = await countStudents(dbpath);
+      res.send(`This is the list of our students\n${output}`);
+    } catch(err) {
+      res.status(500).send(err.message);
+    }
+});
+
+app.listen(1245, () => {
+  console.log('Server is listening on port 1245');
+});
+module.exports = app;
