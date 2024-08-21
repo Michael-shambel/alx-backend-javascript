77949eeb5ec3a397a424d5b91cbf570d95c45250
michael-shambel <Michaelshambel10@gmail.com>
2024-08-21 12:41:58 +0300

    task 5up1

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,7 +1,6 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-
 const app = http.createServer(async (req, res) => {
   if (req.url === '/') {
     res.statusCode = 200;
