a9e70a5329703f86c3171d7b8788c0e355bc1d6b
michael-shambel <Michaelshambel10@gmail.com>
2024-08-21 12:35:24 +0300

    task 5

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -5,7 +5,7 @@ async function countStudents(path) {
     const data = await fs.readFile(path, 'utf8');
     const lines = data.trim().split('\n');
     const students = lines.slice(1).filter((line) => line.trim() !== '');
-    console.log(`Number of students: ${students.length}`);
+    let output = `Number of students: ${students.length}\n`;
     const fields = {};
     students.forEach((student) => {
       const [firstname,, , field] = student.split(',');
@@ -16,8 +16,9 @@ async function countStudents(path) {
     });
 
     for (const [field, names] of Object.entries(fields)) {
-      console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+      output += `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}\n`;
     }
+    return output.trim();
   } catch (err) {
     throw new Error('Cannot load the database');
   }
