f9c25bdb764b4be526733d88f6666f46baa46335
michael-shambel <Michaelshambel10@gmail.com>
2024-08-21 17:17:04 +0300

    task 7 lint;

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -7,9 +7,9 @@ async function countStudents(path) {
   try {
     const data = await fs.readFile(path, 'utf8');
     const lines = data.trim().split('\n');
-    const students = lines.slice(1).filter(line => line.trim() !== '');
+    const students = lines.slice(1).filter((line) => line.trim() !== '');
     const fields = {};
-    students.forEach(student => {
+    students.forEach((student) => {
       const [firstname,, , field] = student.split(',');
       if (!fields[field]) {
         fields[field] = [];
