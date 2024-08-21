602046a657f268d47f3c363790ec2c7778d3a727
michael-shambel <Michaelshambel10@gmail.com>
2024-08-21 12:05:50 +0300

    task 3 updete one line

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -5,6 +5,7 @@ async function countStudents(path) {
     const data = await fs.readFile(path, 'utf8');
     const lines = data.trim().split('\n');
     const students = lines.slice(1).filter((line) => line.trim() !== '');
+    console.log(`Number of students: ${students.length}`);
     const fields = {};
     students.forEach((student) => {
       const [firstname,, , field] = student.split(',');
@@ -13,6 +14,7 @@ async function countStudents(path) {
       }
       fields[field].push(firstname);
     });
+
     for (const [field, names] of Object.entries(fields)) {
       console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
     }
---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+
+const app = http.createServer(async (req, res) => {
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.write('This is the list of our students\n');
+
+    const dbPath = process.argv[2];
+    try {
+      const output = await countStudents(dbPath);
+      res.end(output);
+    } catch (err) {
+      res.statusCode = 500;
+      res.end(err.message);
+    }
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Not Found');
+  }
+});
+
+app.listen(1245, () => {
+  console.log('Server is listening on port 1245');
+});
+module.exports = app;
