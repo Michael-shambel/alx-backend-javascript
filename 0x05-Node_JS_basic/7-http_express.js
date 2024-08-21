249383f974b567252813905d4b88808335611fce
michael-shambel <Michaelshambel10@gmail.com>
2024-08-21 17:21:05 +0300

    task 7

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,31 +1,7 @@
 const express = require('express');
-const fs = require('fs').promises;
+const countStudents = require('./3-read_file_async');
 
 const app = express();
-
-async function countStudents(path) {
-  try {
-    const data = await fs.readFile(path, 'utf8');
-    const lines = data.trim().split('\n');
-    const students = lines.slice(1).filter((line) => line.trim() !== '');
-    const fields = {};
-    students.forEach((student) => {
-      const [firstname,, , field] = student.split(',');
-      if (!fields[field]) {
-        fields[field] = [];
-      }
-      fields[field].push(firstname);
-    });
-
-    let output = `Number of students: ${students.length}\n`;
-    for (const [field, names] of Object.entries(fields)) {
-      output += `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}\n`;
-    }
-    return output.trim();
-  } catch (err) {
-    throw new Error('Cannot load the database');
-  }
-}
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
