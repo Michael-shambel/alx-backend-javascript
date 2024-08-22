9ec69c8da676d94ef958cfcdf0080fc8ccc80a6f
michael-shambel <Michaelshambel10@gmail.com>
2024-08-22 22:13:42 +0300

    task 5

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,29 @@
+const sinon = require('sinon');
+const chai = require('chai');
+const expect = chai.expect;
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', () => {
+    let consoleSpy;
+
+    beforeEach(() => {
+        consoleSpy = sinon.spy(console, 'log');
+    });
+
+    afterEach(() => {
+        consoleSpy.restore();
+    });
+
+    it('should log "The total is: 120" and call console.log once', () => {
+        sendPaymentRequestToApi(100, 20);
+        expect(consoleSpy.calledOnce).to.be.true;
+        expect(consoleSpy.calledWith('The total is: 120')).to.be.true;
+    });
+
+    it('should log "The total is: 20" and call console.log once', () => {
+        sendPaymentRequestToApi(10, 10);
+        expect(consoleSpy.calledOnce).to.be.true;
+        expect(consoleSpy.calledWith('The total is: 20')).to.be.true;
+    });
+});
