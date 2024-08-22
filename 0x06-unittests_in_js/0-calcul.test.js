7fe4681fb7d69aeee066e1c6bf610f62a5747c2f
michael-shambel <Michaelshambel10@gmail.com>
2024-08-22 12:53:22 +0300

    task 0 up4

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,13 +1,24 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', () => {
-  it('should return sum when inputs are two numbers', () =>{
-    assert.strictEqual(calculateNumber(1, 3), 4);
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-  });
+describe('calculateNumber', function () {
+    it('should return the sum of two numbers rounded off', function () {
+        assert.strictEqual(calculateNumber(1, 3), 4);
+        assert.strictEqual(calculateNumber(1, 3.7), 5);
+        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    });
+});
+// const assert = require('assert');
+// const calculateNumber = require('./0-calcul');
+
+// describe('calculateNumber', () => {
+//   it('should return sum when inputs are two numbers', () =>{
+//     assert.strictEqual(calculateNumber(1, 3), 4);
+//     assert.strictEqual(calculateNumber(1, 3.7), 5);
+//     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+//     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+//   });
 
 //   it('should return 5 when inputs are 1 and 3.9', () => {
 //     assert.strictEqual(calculateNumber(1, 3.7), 5);
@@ -20,4 +31,4 @@ describe('calculateNumber', () => {
 //   it('should return 6 when inputs are 1.5 and 3.7', () => {
 //     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
 //   });
-});
+// });
