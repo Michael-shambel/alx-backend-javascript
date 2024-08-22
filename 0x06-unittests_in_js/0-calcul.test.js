ac063b6299d967feaa3baeff475c09684eee3d2a
michael-shambel <Michaelshambel10@gmail.com>
2024-08-22 12:38:35 +0300

    task o up1

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -7,14 +7,14 @@ describe('calculateNumber', () => {
   });
 
   it('should return 5 when inputs are 1 and 3.9', () => {
-    assert.strictEqual(calculateNumber(1, 3.9), 5);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
 
-  it('should return 6 when inputs are 2.2 and 3.7', () => {
-    assert.strictEqual(calculateNumber(2.2, 3.7), 6);
+  it('should return 5 when inputs are 1.2 and 3.7', () => {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
   });
 
-  it('should return 7 when inputs are 2.5 and 3.7', () => {
-    assert.strictEqual(calculateNumber(2.5, 3.7), 7);
+  it('should return 6 when inputs are 1.5 and 3.7', () => {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
 });
