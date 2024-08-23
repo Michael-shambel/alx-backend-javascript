f22f8fa51ce238faebd193626207c3671e29554c
michael-shambel <Michaelshambel10@gmail.com>
2024-08-23 14:07:46 +0300

    task 3

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,25 +1,22 @@
 const fs = require('fs').promises;
 
-async function countStudents(path) {
+async function readDatabase(path) {
   try {
     const data = await fs.readFile(path, 'utf8');
     const lines = data.trim().split('\n');
     const students = lines.slice(1).filter((line) => line.trim() !== '');
-    console.log(`Number of students: ${students.length}`);
     const fields = {};
+
     students.forEach((student) => {
-      const [firstname,, , field] = student.split(',');
+      const [firstname,, , field] = student.trim().split(',');
       if (!fields[field]) {
         fields[field] = [];
       }
       fields[field].push(firstname);
     });
-
-    for (const [field, names] of Object.entries(fields)) {
-      console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
-    }
+    return fields;
   } catch (err) {
     throw new Error('Cannot load the database');
   }
 }
-module.exports = countStudents;
+module.exports = readDatabase;
