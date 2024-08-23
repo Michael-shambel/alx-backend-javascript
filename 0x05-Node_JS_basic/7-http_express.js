345a68f92a811340bed76bec04593b3976ee9b7f
michael-shambel <Michaelshambel10@gmail.com>
2024-08-23 16:21:02 +0300

    task 7

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const fs = require('fs').promises;
+
+const app = express();
+
+async function countStudents(path) {
+  try {
+    const data = await fs.readFile(path, 'utf8');
+    const lines = data.trim().split('\n');
+    const students = lines.slice(1).filter((line) => line.trim() !== '');
+    let output = `Number of students: ${students.length}\n`;
+    const fields = {};
+    students.forEach((student) => {
+      const [firstname,, , field] = student.split(',');
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(firstname);
+    });
+
+    for (const [field, names] of Object.entries(fields)) {
+      output += `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}\n`;
+    }
+    return output.trim();
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  const dbpath = process.argv[2];
+  try {
+    const output = await countStudents(dbpath);
+    res.status(200).send(`This is the list of our students\n${output}`);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+app.listen(1245, () => {
+  console.log('Server is listening on port 1245');
+});
+module.exports = app;
